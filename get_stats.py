a07f4e6f0b4b675e67244f14667a6285cc21caff
Sidhi Adkoli <sidhime@yahoo.co.in>
2020-07-11 09:01:06 -0700

    Basic authentication flow.

--- get_stats.py
+++ get_stats.py
@@ -1,6 +1,38 @@
+#!/usr/bin/env python3
+
 import argparse
+import json
 import logging
 import sys
+import urllib.parse as up
+
+import requests
+
+
+def authenticate():
+    """Get access to user's activities."""
+    # For now, the user needs to authenticate this script through a manual copy-pasting of URLs.
+    with open('.config.json') as fin:
+        config = json.load(fin)
+
+    print("Visit the following URL to authenticate: "
+          f"www.strava.com/oauth/authorize?client_id={config['id']}"
+          "&redirect_uri=http://localhost&response_type=code&scope=activity:read_all")
+    response_url = input("Enter the URL you were redirected to: ")
+    o = up.urlparse(response_url)
+    query = up.parse_qs(o.query)
+    code = query['code']
+
+    # Get refresh and access tokens.
+    exchange_query = {'client_id': config['id'],
+                      'client_secret': config['secret'],
+                      'code': code,
+                      'grant_type': 'authorization_code'}
+    resp = requests.post('https://www.strava.com/api/v3/oauth/token', data=exchange_query)
+    resp.raise_for_status()
+    tokens = resp.json()
+    logging.info("Successfully authenticated %s!", tokens['athlete']['firstname'])
+    return tokens['access_token'], tokens['refresh_token'], tokens['expires_at']
 
 
 def parse_arguments(args):
@@ -18,3 +50,5 @@ def setup_logging(args):
 if __name__ == '__main__':
     args = parse_arguments(sys.argv[1:])
     setup_logging(args)
+
+    access_token, refresh_token, _ = authenticate()
